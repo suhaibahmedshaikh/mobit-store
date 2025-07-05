6bb89ecab2a4254af671f4b1a22fea7791a405e4
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2025-07-05 15:02:31 +0530

    feat: fixed minor bugs in repository for category module

--- server/src/repositories/category-repository.js
+++ server/src/repositories/category-repository.js
@@ -1,20 +1,28 @@
 import { Category } from "../models/category-model.js";
 
 export class CategoryRepository {
-  async addCategory(data) {
-    return await Category.create(data);
+  async addCategory(name) {
+    return await Category.create({ name });
   }
 
   async getAllCategories() {
     return await Category.find({});
   }
 
+  async getCategoryByName(name) {
+    return await Category.findOne({ name });
+  }
+
   async getCategoryById(categoryId) {
     return await Category.findById(categoryId);
   }
 
-  async updateCategory(categoryId, data) {
-    return await Category.findByIdAndUpdate(categoryId, data, { new: true });
+  async updateCategory(categoryId, name) {
+    return await Category.findByIdAndUpdate(
+      categoryId,
+      { name },
+      { new: true }
+    );
   }
 
   async deleteCategory(categoryId) {
