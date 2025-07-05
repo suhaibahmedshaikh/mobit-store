eed66100cc88652cb3a8b7d9e46c39d2a7a6d246
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2025-07-05 14:49:30 +0530

    feat: added model for category

---
+++ server/src/models/category-model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import slugify from "slugify";
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+  },
+  { timestamps: true }
+);
+
+categorySchema.pre("save", function (next) {
+  if (this.isModified("name")) {
+    this.slug = slugify(this.name, { lower: true });
+  }
+  next();
+});
+
+export const Category = mongoose.model("Category", categorySchema);
