bb86bcd2992a332740f2a2595bb2a26ee2f35c37
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2025-07-07 15:03:36 +0530

    feat: connect category module route to main route

--- server/src/routes/v1/v1-routes.js
+++ server/src/routes/v1/v1-routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import productRouter from "./product-routes.js";
+import categoryRouter from "./category-route.js";
 
 const router = express.Router();
 
 router.use("/product", productRouter);
+router.use("/category", categoryRouter);
 
 export default router;
