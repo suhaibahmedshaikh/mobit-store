4373ee1cc43672ff436df020c72b17ea4d14a92c
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2025-07-07 20:28:08 +0530

    feat: added repository layer for cart module

---
+++ server/src/repositories/cart-repository.js
@@ -0,0 +1,27 @@
+import { Cart } from "../models/cart-model.js";
+
+export class CartRepository {
+  async addItemToCart(data) {
+    return await Cart.create(data);
+  }
+
+  async getCartByUser(userId) {
+    return await Cart.findOne({ user: userId }).populate("items.product");
+  }
+
+  async removeItemFromCart(userId, productId) {
+    return await Cart.findOneAndUpdate(
+      { user: userId },
+      { $pull: { items: { product: productId } } },
+      { new: true }
+    ).populate("items.product");
+  }
+
+  async clearCart(userId) {
+    return await Cart.findOneAndUpdate(
+      { user: userId },
+      { items: [] },
+      { new: true }
+    ).populate("items.product");
+  }
+}
