b727802a35b36c3aa313be4b792afcc824d7743c
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2025-07-14 16:06:14 +0530

    added initial layout

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4">
+      © {new Date().getFullYear()} mob-it. All rights reserved.
+    </footer>
+  );
+}
